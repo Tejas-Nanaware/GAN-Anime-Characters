f2b985432b36a0bbb0aba72e45f82590f607c7d7
Tejas-Nanaware <nanawaret@hotmail.com>
2020-04-20 23:35:57 -0500

    Changed network and increased batch size

--- Generative Adversarial Network.py
+++ Generative Adversarial Network.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[21]:
+# In[1]:
 
 
 import numpy as np
@@ -38,47 +38,47 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 # train_gen = train_datagen.flow_from_directory('.', classes=['anime_face'], target_size = (128, 128), 
 #                                               batch_size = 32, color_mode='rgb')
 
-# In[22]:
+# In[2]:
 
 
 # Global Constants
 NOISE = (1,1,100)
-IMAGE_SHAPE = (64,64,3)
+IMAGE_SHAPE = (128,128,3)
 # GAN_STEPS = int(140000 / train_gen.batch_size)
-GAN_STEPS = 250
-BATCH_SIZE = 64
+GAN_STEPS = 1000
+BATCH_SIZE = 256
 
 
-# In[23]:
+# In[3]:
 
 
 def generator_model(noise=NOISE):
     gen_input = Input(shape=noise)
     
-    generator = layers.Dense(32 * 32 * 128, use_bias=False)(gen_input)
+    generator = layers.Dense(16 * 16 * 128, use_bias=False)(gen_input)
     generator = layers.BatchNormalization()(generator)
     generator = layers.LeakyReLU()(generator)
-    generator = layers.Reshape((32, 32, 128))(generator)
+    generator = layers.Reshape((16, 16, 128))(generator)
     
     generator = layers.Conv2DTranspose(filters=256, kernel_size=(5,5), strides=(1,1), use_bias=False, padding='same', kernel_initializer='glorot_uniform')(generator)
     generator = layers.BatchNormalization()(generator)
     generator = layers.LeakyReLU()(generator)
     
-#     generator = layers.Conv2DTranspose(filters=128, kernel_size=(5,5), strides=(2,2), use_bias=False, padding='same', kernel_initializer='glorot_uniform')(generator)
-#     generator = layers.BatchNormalization()(generator)
-#     generator = layers.LeakyReLU()(generator)
+    generator = layers.Conv2DTranspose(filters=256, kernel_size=(5,5), strides=(2,2), use_bias=False, padding='same', kernel_initializer='glorot_uniform')(generator)
+    generator = layers.BatchNormalization()(generator)
+    generator = layers.LeakyReLU()(generator)
     
     generator = layers.Conv2D(filters=128, kernel_size=(5,5), strides=(2,2), use_bias=False, padding='same', kernel_initializer='glorot_uniform')(generator)
     generator = layers.BatchNormalization()(generator)
     generator = layers.LeakyReLU()(generator)
     
-    generator = layers.Conv2DTranspose(filters=64, kernel_size=(5,5), strides=(2,2), use_bias=False, padding='same', kernel_initializer='glorot_uniform')(generator)
+    generator = layers.Conv2DTranspose(filters=128, kernel_size=(5,5), strides=(2,2), use_bias=False, padding='same', kernel_initializer='glorot_uniform')(generator)
     generator = layers.BatchNormalization()(generator)
     generator = layers.LeakyReLU()(generator)
     
-#     generator = layers.Conv2DTranspose(filters=128, kernel_size=(5,5), strides=(2,2), use_bias=False, padding='same', kernel_initializer='glorot_uniform')(generator)
-#     generator = layers.BatchNormalization()(generator)
-#     generator = layers.LeakyReLU()(generator)
+    generator = layers.Conv2DTranspose(filters=128, kernel_size=(5,5), strides=(2,2), use_bias=False, padding='same', kernel_initializer='glorot_uniform')(generator)
+    generator = layers.BatchNormalization()(generator)
+    generator = layers.LeakyReLU()(generator)
     
     generator = layers.Conv2DTranspose(filters=3, kernel_size=(5,5), strides=(2,2), activation='tanh', use_bias=False, padding='same', kernel_initializer='glorot_uniform')(generator)
     
@@ -88,7 +88,7 @@ def generator_model(noise=NOISE):
     return model
 
 
-# In[27]:
+# In[4]:
 
 
 def discriminator_model(image_shape=IMAGE_SHAPE):
@@ -102,13 +102,13 @@ def discriminator_model(image_shape=IMAGE_SHAPE):
     discriminator = layers.LeakyReLU()(discriminator)
     discriminator = layers.Dropout(0.3)(discriminator)
     
-    discriminator = layers.Conv2D(filters=128, kernel_size=(5,5), padding='same', strides=(2,2), kernel_initializer='glorot_uniform', kernel_regularizer=l2(1e-2))(disc_input)
+    discriminator = layers.Conv2D(filters=256, kernel_size=(5,5), padding='same', strides=(2,2), kernel_initializer='glorot_uniform', kernel_regularizer=l2(1e-2))(disc_input)
     discriminator = layers.LeakyReLU()(discriminator)
     discriminator = layers.Dropout(0.3)(discriminator)
     
-#     discriminator = layers.Conv2D(filters=128, kernel_size=(5,5), padding='same', strides=(2,2), kernel_initializer='glorot_uniform', kernel_regularizer=l2(1e-2))(disc_input)
-#     discriminator = layers.LeakyReLU()(discriminator)
-#     discriminator = layers.Dropout(0.3)(discriminator)
+    discriminator = layers.Conv2D(filters=512, kernel_size=(5,5), padding='same', strides=(2,2), kernel_initializer='glorot_uniform', kernel_regularizer=l2(1e-2))(disc_input)
+    discriminator = layers.LeakyReLU()(discriminator)
+    discriminator = layers.Dropout(0.3)(discriminator)
     
 #     discriminator = layers.Conv2D(filters=256, kernel_size=(5,5), padding='same', strides=(2,2), kernel_initializer='glorot_uniform', kernel_regularizer=l2(1e-2))(disc_input)
 #     discriminator = layers.LeakyReLU()(discriminator)
@@ -216,14 +216,14 @@ def discriminator_model(image_shape=IMAGE_SHAPE):
 #     
 #     return model
 
-# In[28]:
+# In[5]:
 
 
 gen_model = generator_model(NOISE)
 gen_model.summary()
 
 
-# In[29]:
+# In[6]:
 
 
 disc_model = discriminator_model()
@@ -231,7 +231,7 @@ disc_model.summary()
 disc_model.trainable = False
 
 
-# In[30]:
+# In[7]:
 
 
 gan_gen_input = Input(shape=NOISE)
@@ -243,7 +243,7 @@ gan_model.compile(optimizer=Adam(lr=1e-4), loss=losses.binary_crossentropy, metr
 gan_model.summary()
 
 
-# In[31]:
+# In[8]:
 
 
 def save_fig(predicted, current_time):
@@ -266,7 +266,7 @@ def save_fig(predicted, current_time):
     plt.close('all')
 
 
-# In[32]:
+# In[9]:
 
 
 def get_real_images(batch_size=BATCH_SIZE, image_shape=IMAGE_SHAPE, data_search_pattern='./anime_face/000*'):
@@ -281,7 +281,7 @@ def get_real_images(batch_size=BATCH_SIZE, image_shape=IMAGE_SHAPE, data_search_
     return batch_images
 
 
-# In[33]:
+# In[10]:
 
 
 def PIL_get_images(batch_size=BATCH_SIZE, image_shape=IMAGE_SHAPE, data_search_pattern='./anime_face/000*'):
@@ -299,7 +299,7 @@ def PIL_get_images(batch_size=BATCH_SIZE, image_shape=IMAGE_SHAPE, data_search_p
     return batch_images
 
 
-# In[34]:
+# In[11]:
 
 
 for file in glob.glob('./GeneratedFigures/*'):
@@ -310,7 +310,7 @@ for file in glob.glob('./GANModels/*'):
         os.remove(file)
 
 
-# In[35]:
+# In[ ]:
 
 
 with open('log.csv', 'w') as log:
