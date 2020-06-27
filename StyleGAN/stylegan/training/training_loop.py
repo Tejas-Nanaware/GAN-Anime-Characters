94c909a7ed655e86d6d5b053495cbb2d9cfccf9a
Tejas-Nanaware <nanawaret@hotmail.com>
2020-06-27 06:55:24 -0500

    Resuming training from 4th tick

--- StyleGAN/stylegan/training/training_loop.py
+++ StyleGAN/stylegan/training/training_loop.py
@@ -145,9 +145,9 @@ def training_loop(
     network_snapshot_ticks  = 1,       # How often to export network snapshots?
     save_tf_graph           = False,    # Include full TensorFlow computation graph in the tfevents file?
     save_weight_histograms  = False,    # Include weight histograms in the tfevents file?
-    resume_run_id           = None,     # Run ID or network pkl to resume training from, None = start from scratch.
+    resume_run_id           = "00000",     # Run ID or network pkl to resume training from, None = start from scratch.
     resume_snapshot         = None,     # Snapshot index to resume training from, None = autodetect.
-    resume_kimg             = 0.0,      # Assumed training progress at the beginning. Affects reporting and training schedule.
+    resume_kimg             = 560.1,      # Assumed training progress at the beginning. Affects reporting and training schedule.
     resume_time             = 0.0):     # Assumed wallclock time at the beginning. Affects reporting.
 
     # Initialize dnnlib and TensorFlow.
